9167b5bc09acfb8132269a826642ed874b959c80
oussema <oghawi220@gmail.com>
2023-05-21 12:33:47 +0100

    file

---
+++ src/assets/index.js
@@ -0,0 +1,84 @@
+import logo from './logo.svg';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import threejs from './tech/threejs.svg';
+
+import meta from './company/meta.png';
+import shopify from './company/shopify.png';
+import starbucks from './company/starbucks.png';
+import tesla from './company/tesla.png';
+
+import carrent from './carrent.png';
+import jobit from './jobit.png';
+import tripguide from './tripguide.png';
+import linkedin from './socialIcons/linkedin.png';
+import sass from './tech/sass.png';
+import logo_letter from './logo_letter.png';
+import code_tag from './socialIcons/code_tag.png';
+import company_icon from './images/company_icon.png';
+import project2 from './images/portfolio/project2.png';
+import music_app from './images/portfolio/music_app.png';
+import project3 from './images/portfolio/project3.jpg';
+import adobexd from './tech/adobexd.png';
+import project5 from './images/portfolio/project5.png';
+import facebook from './socialIcons/facebook.png';
+
+export {
+	linkedin,
+	facebook,
+	music_app,
+	project5,
+	adobexd,
+	company_icon,
+	project2,
+	project3,
+	code_tag,
+	logo_letter,
+	logo,
+	sass,
+	backend,
+	creator,
+	mobile,
+	web,
+	github,
+	menu,
+	close,
+	css,
+	docker,
+	figma,
+	git,
+	html,
+	javascript,
+	mongodb,
+	nodejs,
+	reactjs,
+	redux,
+	tailwind,
+	typescript,
+	threejs,
+	meta,
+	shopify,
+	starbucks,
+	tesla,
+	carrent,
+	jobit,
+	tripguide,
+};
---
+++ src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { socialMediaIcons } from '../constants';
+import { slideIn } from '../utils/motion';
+import { socialLink } from '../constants/iconsURL';
+import { code_tag } from '../assets';
+
+const Footer = () => {
+	return (
+		<div className='flex flex-wrap gap-7'>
+			{socialMediaIcons.map((account, index) => (
+				<motion.div
+					variants={slideIn('left', 'tween', 0.5, 1)}
+					key={index}
+					className='w-full'>
+					<Tilt
+						options={{
+							max: 45,
+							scale: 1,
+							speed: 450,
+						}}
+						className='bg-tertiary p-5 rounded-2xl'>
+						<div className='mt-5'>
+							<h3 className='text-white font-bold text-[24px] text-center'>
+								{account.name}
+							</h3>
+						</div>
+						<br />
+						<div className='flex justify-center items-center'>
+							{account.Icons.map((item, i) => (
+								<p
+									key={i}
+									className='w-[6rem] h-[8vh] flex justify-center items-center text-secondary text-[14px]'>
+									<div
+										onClick={() => socialLink(item)}
+										className='black-gradient w-14 h-14 rounded-full flex flex-row justify-center items-center cursor-pointer'>
+										<img
+											src={item}
+											alt='source code'
+											className='w-10 h-10 object-contain'
+										/>
+									</div>
+								</p>
+							))}
+						</div>
+						<br />
+
+						<div className='mt-4 flex justify-center flex-wrap gap-2'>
+							{account.tags.map((tag) => (
+								<p
+									key={`${name}-${tag.name}`}
+									className={`text-[14px] ${tag.color}`}>
+									#{tag.name}
+								</p>
+							))}
+						</div>
+					</Tilt>
+				</motion.div>
+			))}
+		</div>
+	);
+};
+
+export default SectionWrapper(Footer, 'footer');
